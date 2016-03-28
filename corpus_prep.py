61f0ab432f8d3f370fd2bf13f0f0c933e724650c
edit01-lap <jason.hwee@gmail.com>
2016-03-28 14:35:52 -0400

    documentation

--- corpus_prep.py
+++ corpus_prep.py
@@ -4,15 +4,19 @@ import re
 import io
 
 """
-
+The clincal_study.txt file is what you see is what you get. All of the data for a single trial spans multiple lines in the same file. We therefore need to scan through the lines up until we hit the next study. We can do this pattern matching for the NCT_ID where NCT_ID represents the start of a trial. Text/lines preceding an NCT_ID should be considered part of the previous study.
 """
 
 def prep(in_file):
     NCT = re.compile('^NCT[0-9]+\|')
     
     with io.open(in_file, 'r', encoding="utf-8-sig") as source:        
-        header = source.readline().strip().split("|")        
+        header = source.readline().strip().split("|") # Read in the head line and store it in a var
         print ("|").join(header).encode("utf-8")
+        
+        # Scan through each line and until we hit an NCT_ID. Until we do, append lines to the cursor list.
+        # When we do hit the next NCT_ID, join the resulting stored lines up until that point into one "|" delimited line.
+        # Reset the cursor list to [] or empty and repeat for the next NCT record.
         cur = []
         for line in source:
             line = line.strip()    
@@ -21,21 +25,15 @@ def prep(in_file):
                 if mo:                    
                     if cur != []:                        
                         trial = ("").join(cur).split("|")
-                        """
-                        if len(trial) != 45:
-                            print line
-                            print trial[0], len(trial)
-                        """
                         print ("|").join(trial).encode("utf-8")
 
-                        #for item in zip(header, trial):
-                        #    print item
                         cur = []
                     cur.append(line)                    
                 else:
                     cur.append(line)
     return None
-
+# INPUT: raw/clinical_study.txt
+# OUTPUT: The output should be written out to a file where each line is a single pipe "|" delimited study record
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
